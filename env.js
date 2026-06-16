de94d86720a4dd74a502d974965d236c0fe95bd2
raven <lenoirrrrr>
2026-06-16 15:07:48 +0700

    fix bug

--- env.js
+++ env.js
@@ -1,7 +1,7 @@
 window.__APP_ENV__ = Object.assign(
     {
         APP_API_BASE_URL: "https://projectkp-shendydafiq-backend-production.up.railway.app/api",
-        APP_AUTH_BASE_URL: "http://localhost:3000/api/auth",
+        APP_AUTH_BASE_URL: "https://projectkp-shendydafiq-backend-production.up.railway.app/api",
     },
     window.__APP_ENV__ || {}
 );
